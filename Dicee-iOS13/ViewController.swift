633e6d7d23b43ab2ea06d2ca587cdc6c175d993b
amynata <amynata.nepomuceno@gmail.com>
2020-04-13 22:37:14 -0400

    change rollButtonPressed action to increase 1 number on the left and decrease 1 number on the right.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -17,25 +17,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        //who           what   value
-    
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        //Image Literal (UIImage) alows us to define what the actual displayed image will be
+        
     }
 
     //ABAction triggers the code inside the curly braces
     //IBAction - interaction with UI leads to action in code
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped")
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [leftDiceNumber]
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        leftDiceNumber = leftDiceNumber + 1
+        
+        
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [rightDiceNumber]
+        
+        rightDiceNumber = rightDiceNumber - 1
+        
     }
     
 }
