6e1fb38b62abf4041e6c4ccb7d0055d739c17b8a
amynata <amynata.nepomuceno@gmail.com>
2020-04-13 21:59:05 -0400

    link UI elements to code and alter diceImageView

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //add all elements to the view controler before viewDidLoad
+    //hold ctrl and drag the element to the code
+    //an IBOutlet allows me to reference a UI Element
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        //who           what   value
+    
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        //Image Literal (UIImage) alows us to define what the actual displayed image will be
     }
 
 
